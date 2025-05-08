e8a9bf515d57c3384792b3431de304f7a95fabc0
SRB77 <soumyach144@gmail.com>
2025-05-08 22:08:46 +0530

    End of Class 4 with bit UseState

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-
+import { useState } from 'react';
 const App = () => {
-  let profiles = [
-    { name: 'John Doe', age: 30 }, 
-    { name: 'Jane Smith', age: 25 }, 
-    { name: 'Alice Johnson', age: 28 }
-  ]
-   const updatedProfile = profiles.map((profile,index) => {
-    return (
-      <li key={index}>
-        <h3>Name : {profile.name}</h3>  
-        <small>Age : {profile.age}</small>
-      </li>
-    )
-   });
+  let [username,setUserName] = useState("Chandan");
+  // let username = "chandan";
+  // console.log(username);
+  // const changeHandler = ()=>{
+  //   username = "Soumyaranjan"
+  //   console.log(username);
+  // }
+  const changeHandler = ()=>{
+    setUserName("Soumyaranjan")
+  }
+  // console.log(username)  this is working sync and setUserName is working async . 
+
   return (
-    <>
-      <h1>React App</h1>
-     <ol>
-      {updatedProfile}
-     </ol>
-    </>
+    <div>
+      <h1>Username is : </h1>
+      <h2>{username}</h2>
+      <button onClick={changeHandler}>Click to Change</button>
+    </div>
   )
 }
 
