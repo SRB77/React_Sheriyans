13f5667263508b6e52d6f3157b28e78a8666d75f
SRB77 <soumyach144@gmail.com>
2025-05-13 10:10:25 +0530

    Class5 two way binding and first 45 minitue .

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useState } from "react";
 const App = () => {
-  let [username,setUserName] = useState("Chandan");
-  // let username = "chandan";
-  // console.log(username);
-  // const changeHandler = ()=>{
-  //   username = "Soumyaranjan"
-  //   console.log(username);
-  // }
-  const changeHandler = ()=>{
-    setUserName("Soumyaranjan")
-  }
-  // console.log(username)  this is working sync and setUserName is working async . 
+  const [user, setUser] = useState([
+    { name: "chandan", age: 20 },
+    { name: "soumya", age: 10 },
+    { name: "heillo", age: 23 },
+  ]);
+  const [fullName, setFullname] = useState("");
+  const [age, setAge] = useState("");
 
+  const renderUser = user.map((obj, index) => {
+    return <li key={index}>{obj.name}</li>;
+  });
+  const submitHandler =  (e)=>{
+    e.preventDefault();
+    let newUser = {fullName,age}
+    console.log(newUser);
+  }
   return (
     <div>
-      <h1>Username is : </h1>
-      <h2>{username}</h2>
-      <button onClick={changeHandler}>Click to Change</button>
+      <div className="registerform">
+        <h1>Register USER</h1>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="fullName"
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
+            value={fullName}
+          />
+          <input
+            type="number"
+            placeholder="phone Number"
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+            value={age}
+          />
+          <button>submit</button>
+        </form>
+        <hr />
+      </div>
+      <h1>USER DATA : </h1>
+      <ol>{renderUser}</ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
